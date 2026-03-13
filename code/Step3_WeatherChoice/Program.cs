c05ee32675bfa69341882fb0b7095afc694497fd
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-03-13 14:14:04 +0300

    Step 3: conditional algorithm and if/else in C#

---
+++ code/Step3_WeatherChoice/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Choose clothes by weather");
+System.Console.Write("Enter temp (C): ");
+int temperature = Convert.ToInt32(Console.ReadLine());
+if (temperature >= 20)
+{
+    System.Console.WriteLine("Bring summer clothes");
+}
+else
+{
+    System.Console.WriteLine("Bring winter clothes");
+}
+System.Console.WriteLine("Have a nice day!");
