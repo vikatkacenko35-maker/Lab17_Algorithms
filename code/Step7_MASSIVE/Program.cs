f67195023f3bddf23a8d1f3bec7b6ac42f1bac5f
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-03-20 10:28:45 +0300

    Step 7 arrays

---
+++ code/Step7_MASSIVE/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] marks = new int[] { 5, 4, 3, 2, 4 };
+foreach (int i in marks)
+{
+    System.Console.WriteLine($"Оценка: {i}");
+}
+int maxim = 0;
+foreach (int item in marks)
+{
+    if (item > maxim)
+    {
+        maxim = item;
+    }
+}
+System.Console.WriteLine(maxim);
